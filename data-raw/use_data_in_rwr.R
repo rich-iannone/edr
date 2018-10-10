66e5b57eafe692ddc1921b6382bd148260219058
Richard Iannone <riannone@me.com>
2018-10-10 13:11:31 -0700

    Add script to add `winniweather` dataset

---
+++ data-raw/use_data_in_rwr.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(usethis)
+
+# This script uses `usethis::use_data()` to add data to the package
+
+
+#
+# The `winniweather` dataset
+#
+# This comes from the Environment Canada Weather Information website https://weather.gc.ca/
+# The downloaded CSV data was processed and reduced in content (both in rows and columns)
+# and saved here as `winniweather.csv`
+#
+
+winniweather <-
+  read_csv("./data-raw/winniweather.csv", col_types = "iiicdiiid") %>%
+  mutate(yearmonth = paste(year, month, sep = "-")) %>%
+  select(yearmonth, everything(), -year, -month) %>%
+  filter(time %in% c("00:00", "06:00", "12:00", "18:00")) %>%
+  select(-rh_pct, -wd, -ws, -stn_p) %>%
+  spread(time, temp_c) %>%
+  rename(
+    temp00_00 = `00:00`,
+    temp06_00 = `06:00`,
+    temp12_00 = `12:00`,
+    temp18_00 = `18:00`)
+
+# Incorporate the `winniweather` tibble as package data
+use_data(
+  winniweather,
+  internal = FALSE, overwrite = TRUE)
