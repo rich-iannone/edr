7fbd515e9ef21f5ab9ce0d9cc1321836b5f06e17
Richard Iannone <riannone@me.com>
2019-02-02 15:40:17 -0500

    Modify data-processing R script

--- data-raw/zz-datasets_ext.R
+++ data-raw/zz-datasets_ext.R
@@ -1,11 +1,13 @@
 library(usethis)
 
 # Source all of the datasets
+source("data-raw/02-sw.R")
 source("data-raw/03-dmd.R")
 source("data-raw/04-winniweather.R")
 
 # Create external datasets
 use_data(
+  sw,
   dmd,
   winniweather,
   internal = FALSE, overwrite = TRUE
