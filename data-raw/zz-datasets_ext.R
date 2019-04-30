78a756e15bededfdd9ba2f38779d258c86598815
Richard Iannone <riannone@me.com>
2019-04-29 21:25:01 -0400

    Modify the `zz-datasets_ext.R` script

--- data-raw/zz-datasets_ext.R
+++ data-raw/zz-datasets_ext.R
@@ -1,14 +1,18 @@
 library(usethis)
 
+# This script uses `usethis::use_data()` to add data to the package
+
 # Source all of the datasets
 source("data-raw/02-sw.R")
 source("data-raw/03-dmd.R")
 source("data-raw/04-winniweather.R")
+source("data-raw/05-us_cities.R")
 
 # Create external datasets
 use_data(
   sw,
   dmd,
   winniweather,
+  us_cities,
   internal = FALSE, overwrite = TRUE
 )
