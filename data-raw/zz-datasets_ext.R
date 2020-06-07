4d28044d3c1e2319a09f2f104b002ae75ca8ce50
Richard Iannone <riannone@me.com>
2020-06-07 03:55:17 -0400

    Update zz-datasets_ext.R

--- data-raw/zz-datasets_ext.R
+++ data-raw/zz-datasets_ext.R
@@ -11,6 +11,8 @@ source("data-raw/06-nycweather.R")
 source("data-raw/07-sales.R")
 source("data-raw/08-resto_reviews.R")
 source("data-raw/09-german_cities.R")
+source("data-raw/10a-employment.R")
+source("data-raw/10b-rainfall.R")
 
 # Create external datasets
 use_data(
@@ -22,6 +24,8 @@ use_data(
   sales,                # 07
   resto_reviews,        # 08
   german_cities,        # 09
+  employment,           # 10a
+  rainfall,             # 10b
   internal = FALSE,
   overwrite = TRUE
 )
